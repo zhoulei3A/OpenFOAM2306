d6652c622b23dc7aa78de3321880cf4159e83131
Mark Olesen <Mark.Olesen@esi-group.com>
2023-09-05 18:23:58 +0200

    ENH: reduce overhead of masterOFstream
    - use OCharStream instead of OStringStream to copying of char data.
    
    - direct non-blocking send/recv with probing instead of
      PstreamBuffers, which reduces the memory footprint and avoids
      serialization/de-serialization of char data.
    
    - use polling dispatch to write file content as it becomes available
      (to improve communication and IO overlap).

--- src/OpenFOAM/db/IOstreams/Fstreams/masterOFstream.C
+++ src/OpenFOAM/db/IOstreams/Fstreams/masterOFstream.C
@@ -29,7 +29,7 @@ License
 #include "masterOFstream.H"
 #include "OFstream.H"
 #include "OSspecific.H"
-#include "PstreamBuffers.H"
+#include "Pstream.H"
 #include "masterUncollatedFileOperation.H"
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
@@ -37,11 +37,10 @@ License
 void Foam::masterOFstream::checkWrite
 (
     const fileName& fName,
-    const char* str,
-    std::streamsize len
+    const UList<char>& charData
 )
 {
-    if (!len)
+    if (charData.empty())
     {
         // Can probably skip all of this if there is nothing to write
         return;
@@ -63,10 +62,8 @@ void Foam::masterOFstream::checkWrite
             << exit(FatalIOError);
     }
 
-    // Use writeRaw() instead of writeQuoted(string,false) to output
-    // characters directly.
-
-    os.writeRaw(str, len);
+    // Use writeRaw() to write characters directly
+    os.writeRaw(charData.cdata(), charData.size_bytes());
 
     if (!os.good())
     {
@@ -77,20 +74,21 @@ void Foam::masterOFstream::checkWrite
 }
 
 
-void Foam::masterOFstream::checkWrite
-(
-    const fileName& fName,
-    const std::string& s
-)
-{
-    checkWrite(fName, s.data(), s.length());
-}
-
-
 void Foam::masterOFstream::commit()
 {
+    // Retrieve serialized content
+    List<char> charData;
+    OCharStream::swap(charData);
+
     if (UPstream::parRun())
     {
+        // Discard content if not writing.
+        // Ensures nothing will be communicated
+        if (!writeOnProc_)
+        {
+            charData.clear();
+        }
+
         List<fileName> filePaths(UPstream::nProcs(comm_));
         filePaths[UPstream::myProcNo(comm_)] = pathName_;
         Pstream::gatherList(filePaths, UPstream::msgType(), comm_);
@@ -107,63 +105,105 @@ void Foam::masterOFstream::commit()
         {
             if (UPstream::master(comm_) && writeOnProc_)
             {
-                checkWrite(pathName_, this->str());
+                checkWrite(pathName_, charData);
             }
-
-            this->reset();
             return;
         }
 
-        // Different files
-        PstreamBuffers pBufs(comm_, UPstream::commsTypes::nonBlocking);
+        // Different files.
+        // Would be slightly easier if we didn't support writeOnProc
+        // but instead of using listGatherValues<bool>() to manage
+        // things on the master, we simply check based on the received
+        // size and only write non-zero file content.
+        // This avoids an additional reduction.
 
-        if (!UPstream::master(comm_))
+        const label startOfRequests = UPstream::nRequests();
+
+        if (UPstream::is_subrank(comm_))
         {
-            if (writeOnProc_)
-            {
-                // Send buffer to master
-                string s(this->str());
-
-                UOPstream os(UPstream::masterNo(), pBufs);
-                os.write(s.data(), s.length());
-            }
-            this->reset();  // Done with contents
+            // Send to master. When (!writeOnProc_) it is zero-sized.
+            UOPstream::write
+            (
+                UPstream::commsTypes::nonBlocking,
+                UPstream::masterNo(),
+                charData.cdata(),
+                charData.size_bytes(),
+                comm_
+            );
         }
-
-        pBufs.finishedGathers();
-
-
-        if (UPstream::master(comm_))
+        else if (UPstream::master(comm_))
         {
+            List<List<char>> procData(UPstream::nProcs(comm_));
+
+            const UPstream::rangeType recvProcs = UPstream::subProcs(comm_);
+
+            for (const int proci : recvProcs)
+            {
+                List<char>& procSlice = procData[proci];
+
+                // Probe the incoming message source/size
+                std::pair<int, int> probed =
+                    UPstream::probeMessage
+                    (
+                        UPstream::commsTypes::blocking,
+                        proci,
+                        UPstream::msgType(),
+                        comm_
+                    );
+
+                procSlice.resize_nocopy(probed.second);
+
+                // Receive content (can also be zero-sized)
+                UIPstream::read
+                (
+                    UPstream::commsTypes::nonBlocking,
+                    proci,
+                    procSlice.data(),
+                    procSlice.size_bytes(),
+                    UPstream::msgType(),
+                    comm_
+                );
+            }
+
             if (writeOnProc_)
             {
                 // Write master data
-                checkWrite(filePaths[UPstream::masterNo()], this->str());
+                checkWrite(filePaths[UPstream::masterNo()], charData);
             }
-            this->reset();  // Done with contents
 
-
-            // Allocate large enough to read without resizing
-            List<char> buf(pBufs.maxRecvCount());
-
-            for (const int proci : UPstream::subProcs(comm_))
+            // Poll for completed receive requests and dispatch
+            DynamicList<int> indices(recvProcs.size());
+            while
+            (
+                UPstream::waitSomeRequests
+                (
+                    startOfRequests,
+                    recvProcs.size(),
+                   &indices
+                )
+            )
             {
-                const std::streamsize count(pBufs.recvDataCount(proci));
-
-                if (count)
+                for (const int idx : indices)
                 {
-                    UIPstream is(proci, pBufs);
+                    const int proci = recvProcs[idx];
 
-                    is.read(buf.data(), count);
-                    checkWrite(filePaths[proci], buf.cdata(), count);
+                    if (!procData[proci].empty())
+                    {
+                        // Write non-empty sub-proc data
+                        checkWrite(filePaths[proci], procData[proci]);
+                    }
+
+                    // Eager cleanup?
+                    // procData[proci].clear();
                 }
             }
         }
+
+        UPstream::waitRequests(startOfRequests);
     }
     else
     {
-        checkWrite(pathName_, this->str());
-        this->reset();
+        checkWrite(pathName_, charData);
     }
 
     // This method is only called once (internally)
@@ -183,7 +223,7 @@ Foam::masterOFstream::masterOFstream
     const bool writeOnProc
 )
 :
-    OStringStream(streamOpt),
+    OCharStream(streamOpt),
     pathName_(pathName),
     atomic_(atomic),
     compression_(streamOpt.compression()),
